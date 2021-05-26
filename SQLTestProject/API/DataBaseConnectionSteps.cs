1d9ac3217b2daa8a16a962968273bd3033b691b0
Sergei Pianykh <pyanykhs@gmail.com>
2021-05-26 14:56:25 +0300

    created Feature File

---
+++ SQLTestProject/API/DataBaseConnectionSteps.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace SQLTestProject.API
+{
+    [Binding]
+    public class DataBaseConnectionSteps
+    {
+        private readonly SqlHelper _sqlHelper = new SqlHelper("Shop");
+
+        [Given(@"Establish a database connection")]
+        public void GivenEstablishADatabaseConnection()
+        {
+            _sqlHelper.OpenConnection();
+        }
+
+        [When(@"I create row in table with data")]
+        public void WhenICreateRowInTableWithData(Table table)
+        {
+            var productModels = table.CreateSet<ProductModel>().ToList();
+
+            _sqlHelper.Insert("Products",
+                new Dictionary<string, string> {
+                    { "Id", $"{ productModels[0].Id }" },
+                    { "Name", $"'{ productModels[0].Name }'" },
+                    { "Count", $"{ productModels[0].Count }" }
+                });
+        }
+
+        [Then(@"Row created with data")]
+        public void ThenRowCreatedWithData(Table table)
+        {
+            var productModels = table.CreateSet<ProductModel>().ToList();
+
+            _sqlHelper.IsRowExistedInTable("Products",
+                new Dictionary<string, string> {
+                    { "Id", $"{ productModels[0].Id }" },
+                    { "Name", $"'{ productModels[0].Name }'" },
+                    { "Count", $"{ productModels[0].Count }" }
+                });
+        }
+
+        public class ProductModel
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string Count { get; set; }
+        }
+    }
+}
---
+++ SQLTestProject/Features/SpecFlowFeature1.feature
@@ -0,0 +1,15 @@
+﻿@db
+Feature: DataBase connection
+	Simple calculator for adding two numbers
+
+Scenario: It is possible to create row in database table
+	Given Establish a database connection
+	When I create row in table with data
+	| id   | name   | count   |
+	| <id> | <name> | <count> |
+	Then Row created with data
+	| id   | name   | count   |
+	| <id> | <name> | <count> |
+Examples:
+	| id | name     | count |
+	| 1  | testName | 123   |
